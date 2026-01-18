e098c76764fb9b5d4d3e98fbb990f66fef1070af
Shivam30Mishra <theshivammishra10@gmail.com>
2026-01-18 11:49:05 +0530

    defined UserModel and exported it by default

---
+++ models/user.model.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+interface IUser {
+  _id      : mongoose.Types.ObjectId;
+  name     : string;
+  email    : string;
+  password : string;
+  mobile   : string;
+  role     : "user" | "deliveryBoy" | "admin";
+}
+
+const userSchema = new mongoose.Schema<IUser>({
+  name     : { type : String, required : true },
+  email    : { type : String, required : true, unique : true },
+  password : { type : String, required : true },
+  mobile   : { type : String, required : false },
+  role     : { type : String, enum : ["user", "deliveryBoy", "admin"], default : "user" }
+},{ timestamps : true }) // createdAt and updatedAt
+
+const UserModel = mongoose.models.User || mongoose.model<IUser>("User", userSchema)
+
+export default UserModel
