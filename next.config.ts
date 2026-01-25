32bc3adea877d3214f3defad9ef9f41a0c158eda
Shivam30Mishra <theshivammishra10@gmail.com>
2026-01-25 18:41:12 +0530

    updated the next.config to accept images from hostname : unsplash

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
-      {hostname : "lh3.googleusercontent.com"},
       {
+        protocol: "https",
         hostname: "images.unsplash.com",
-        protocol: "https"
+        pathname: "/**",
       },
       {
+        protocol: "https",
         hostname: "res.cloudinary.com",
-      }
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
+      },
     ],
   },
 };
