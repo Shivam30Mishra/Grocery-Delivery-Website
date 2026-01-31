edb6cdc6be2326bcc5f21299126f314edb5e0ee4
Shivam30Mishra <theshivammishra10@gmail.com>
2026-01-31 23:33:29 +0530

    api for fetching the orders placed by the user

--- app/api/auth/admin/get-orders/route.ts
+++ app/api/auth/admin/get-orders/route.ts
@@ -5,7 +5,7 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(req:NextRequest){
   try{
     await connectDB()
-    const orders = await OrderModel.find({}).populate("user")
+    const orders = await OrderModel.find({}).populate("user assignedDeliveryBoy").sort({createdAt : -1})
     return NextResponse.json({orders},{status:200})
   }catch(error){
     console.log(error)
