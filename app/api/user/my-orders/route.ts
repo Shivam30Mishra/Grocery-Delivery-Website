21075a80609912bf014645970ddd7efe70254380
Shivam30Mishra <theshivammishra10@gmail.com>
2026-01-31 23:26:57 +0530

    fixed the issue of fetching my-order of user api

--- app/api/user/my-orders/route.ts
+++ app/api/user/my-orders/route.ts
@@ -3,20 +3,20 @@ import { auth } from "@/auth";
 import OrderModel from "@/models/order.model";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req:NextRequest){
+export async function GET(req: NextRequest) {
   try {
     await connectDB()
     const session = await auth()
-    if(!session){
-      return NextResponse.json({error:"Unauthorized"}, {status:401})
+    if (!session) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 })
     }
-    const orders = await OrderModel.find({user:session?.user?.id}).populate("user")
-    if(!orders){
-      return NextResponse.json({error:"No orders found"}, {status:400})
+    const orders = await OrderModel.find({ user: session?.user?.id }).populate("user assignedDeliveryBoy").sort({ createdAt: -1 })
+    if (!orders) {
+      return NextResponse.json({ error: "No orders found" }, { status: 400 })
     }
-    return NextResponse.json({orders}, {status:200})
+    return NextResponse.json({ orders }, { status: 200 })
   } catch (error) {
-    console.log(error) 
-    return NextResponse.json({error:"Internal Server Error"}, {status:500})
+    console.log(error)
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 })
   }
 }
